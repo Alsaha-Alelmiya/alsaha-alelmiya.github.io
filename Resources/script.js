cfd86950c0cb66625413ce40d7f0f1c7f6a1f746
Alsaha-Alelmiya <student.abdullah.faiz.alshehri@gmail.com>
2024-01-18 08:56:35 +0300

    editing 4 vocabularies of unit7 mg 2.. s.o.s

--- Resources/script.js
+++ Resources/script.js
@@ -1,41 +1,41 @@
 setTimeout(function(){
-document.querySelector('.loader').style.color="#000";
+  document.querySelector('.loader').style.color = "#000";
 }, 2900);
 
-var content = document.querySelectorAll('.content');
+var contentElements = document.querySelectorAll('.content');
 
-for(i = 0; i < content.length; i++){
-content[i].style.visibility ="hidden";
+for (var i = 0; i < contentElements.length; i++) {
+  contentElements[i].style.visibility = "hidden";
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  for (i = 0; i < content.length; i++) {
-    content[i].style.visibility = "hidden";
+  for (var i = 0; i < contentElements.length; i++) {
+    contentElements[i].style.visibility = "hidden";
   }
 });
 
 window.addEventListener("load", function() {
-  var loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
+  var loadTime = window.performance.timing.loadEventEnd - window.performance.timing.navigationStart;
 
   if (loadTime < 1250) {
     window.setTimeout(function() {
       var loaderBox = document.getElementById("loaderBox");
       loaderBox.style.opacity = "0";
       loaderBox.style.display = "none";
-      var content = document.getElementsByClassName("content");
-      for (var i = 0; i < content.length; i++) {
-        content[i].style.visibility = "visible";
-        content[i].style.animation = "fade-in 1s ease-in-out";
+      var contents = document.getElementsByClassName("content");
+      for (var i = 0; i < contents.length; i++) {
+        contents[i].style.visibility = "visible";
+        contents[i].style.animation = "fade-in 1s ease-in-out";
       }
     }, 3500);
   } else {
     var loaderBox = document.getElementById("loaderBox");
     loaderBox.style.opacity = "0";
     loaderBox.style.display = "none";
-    var content = document.getElementsByClassName("content");
-    for (var i = 0; i < content.length; i++) {
-      content[i].style.visibility = "visible";
-      content[i].style.animation = "fade-in 1s ease-in-out";
+    var contents = document.getElementsByClassName("content");
+    for (var i = 0; i < contents.length; i++) {
+      contents[i].style.visibility = "visible";
+      contents[i].style.animation = "fade-in 1s ease-in-out";
     }
   }
 });
